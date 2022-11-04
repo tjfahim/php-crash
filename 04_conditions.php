86b95f970e655a870a8c266483c7ecbcbe405d96
Fahim Islam <tjfahim1997@gmail.com>
2022-11-04 22:56:59 +0600

    basic conditions

---
+++ 04_conditions.php
@@ -0,0 +1,27 @@
+<?php 
+
+$test=[];
+
+// if(!empty($test)){
+//     echo $test[0];
+// }
+// else echo 'empty';
+
+// $see = !empty($test) ? $test[0] : 'empty';
+// echo $see
+// echo $test[0] ?? 'see' ;
+
+// $sc = 'no';
+
+// switch($sc){
+//     case 'hmm':
+//         echo 'case hmm';
+//         break;
+//     case 'no':
+//         echo 'case no';
+//         break;
+//     default:
+//         echo 'nothing';
+
+// }
+?>
