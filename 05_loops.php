c84f5984156e9069c43f1ed50e76cf5d18bb8545
Fahim Islam <tjfahim1997@gmail.com>
2022-11-04 23:14:27 +0600

    basic loops

---
+++ 05_loops.php
@@ -0,0 +1,33 @@
+<?php  
+
+// for($i=0;$i<=10;$i++){
+//     echo $i;
+// }
+$i=0;
+// while($i<=10){
+//     echo $i;
+//     $i++;
+// }
+// do{
+//     echo $i;
+//     $i++;
+// }
+// while($i<=10);
+
+$test=[3,4,5,6,13];
+// for($i=0;$i<=count($test);$i++){
+//     echo $test[$i];
+// }
+// foreach($test as $t){
+//     echo $t ;
+// }
+
+// $person = [
+//     'name'=>'fahim',
+//     'age'=>24
+// ];
+
+// foreach($person as $k => $v){
+//     echo "$k - $v<br>";
+// }
+?>
