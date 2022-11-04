93c1799fd80d932a1fc38bb31a38a7a82c69b594
Fahim Islam <tjfahim1997@gmail.com>
2022-11-04 22:27:38 +0600

    varible basic

---
+++ 02_variable.php
@@ -0,0 +1,12 @@
+<?php
+$name = 'fahim';
+$age = 24;
+$student = true;
+$float = 30.44;
+// var_dump($float);
+// echo "my name is $name . And age is $age ";
+
+// define('Name','fahim');
+// echo Name;
+
+?>
