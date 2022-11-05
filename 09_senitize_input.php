aab4b50074786cadc4bd2f094d363a5f4fb73689
Fahim Islam <tjfahim1997@gmail.com>
2022-11-05 23:53:41 +0600

    htmlspecialchars and filtersanitizespecialchars use

---
+++ 09_senitize_input.php
@@ -0,0 +1,22 @@
+<?php 
+if(isset($_POST['submit'])){
+    // echo htmlspecialchars($_POST['name']);
+    echo filter_input(INPUT_POST, 'name',FILTER_SANITIZE_SPECIAL_CHARS);
+    echo $_POST['age'];
+}
+
+
+?>
+
+
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+    <div>
+        <label for="name">Name:</label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label for="age">Age:</label>
+        <input type="text" name="age">
+    </div>
+    <input type="submit" value="Submit" name="submit">
+</form>
